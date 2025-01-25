08a7372ce11cb27e0b647759f47a4cea0b6cf4d1
Maelrox <oscarquiroz@gmail.com>
2025-01-25 10:39:01 -0500

    fixing image links on tsx

--- src/components/modules/login/AuthPage.tsx
+++ src/components/modules/login/AuthPage.tsx
@@ -21,7 +21,7 @@ const AuthPage = () => {
       <div className="relative min-h-screen w-full bg-white">
         <div className="absolute bottom-0 right-0 hidden md:block h-[500px] overflow-hidden">
           <img
-            src="/src/assets/login_doctor.png"
+            src="/login_doctor.png"
             alt="Doctor silhouette"
             className="object-cover h-full w-auto"
           />
@@ -29,7 +29,7 @@ const AuthPage = () => {
 
         <div className="absolute top-0 left-0 right-0 h-[275px] overflow-hidden md:hidden">
           <img
-            src="/src/assets/login_doctor.png"
+            src="/login_doctor.png"
             alt="Doctor silhouette"
             className="object-cover w-full h-full opacity-50"
           />
@@ -39,7 +39,7 @@ const AuthPage = () => {
           <div className="w-full max-w-lg space-y-6 bg-white p-8 shadow-xl">
             <div className="flex justify-center">
               <img
-                src="/src/assets/logo-pet-care-suite.png"
+                src="/logo-pet-care-suite.png"
                 alt="Pet Care Suite Logo"
                 className="w-48 md:w-64"
               />
